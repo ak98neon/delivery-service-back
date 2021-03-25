19522c2471de22bf11c136cc910a0d43352fb642
Artem Kudria <a.kudria@advcash.com>
2021-03-25 23:37:38 +0200

    Init Project

---
+++ src/main/java/com/hashcorp/delivery/DeliveryServiceBackendApplication.java
@@ -0,0 +1,13 @@
+package com.hashcorp.delivery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeliveryServiceBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeliveryServiceBackendApplication.class, args);
+	}
+
+}
