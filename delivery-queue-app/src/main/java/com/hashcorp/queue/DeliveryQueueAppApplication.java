7a81032b728f12ae499149cdfade511cdb41d428
Artem Kudria <a.kudria@advcash.com>
2021-03-26 11:55:52 +0200

    Resovle dependencies

---
+++ delivery-queue-app/src/main/java/com/hashcorp/queue/DeliveryQueueAppApplication.java
@@ -0,0 +1,13 @@
+package com.hashcorp.queue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeliveryQueueAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeliveryQueueAppApplication.class, args);
+	}
+
+}
---
+++ delivery-queue-app/src/test/java/com/hashcorp/queue/DeliveryQueueAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hashcorp.queue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliveryQueueAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
