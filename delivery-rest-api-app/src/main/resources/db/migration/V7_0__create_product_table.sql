6783020a95dc0e16c4af2d575631060f3eeb0a53
Artem Kudria <a.kudria@advcash.com>
2021-03-26 20:11:52 +0200

    Fix migration product file

--- delivery-rest-api-app/src/main/resources/db/migration/V7_0__create_product_table.sql
+++ delivery-rest-api-app/src/main/resources/db/migration/V7_0__create_product_table.sql
@@ -3,7 +3,7 @@ DROP TABLE IF EXISTS permission CASCADE;
 CREATE TABLE product
 (
 	id          BIGSERIAL PRIMARY KEY,
-	name        VARCHAR(60) UNIQUE NOT NULL,
+	name        VARCHAR(60) NOT NULL,
 	description VARCHAR(128),
 	imgUrl      VARCHAR(128) NOT NULL
 );
